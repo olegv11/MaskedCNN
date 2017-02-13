fb404f3e1c3aadbf14561ddcf221fd6761caa891
Oleg Sikorskiy <olegsik@gmail.com>
2017-02-13 16:50:38 +0000

    Activation functions

--- include/maskedcnn/Activation.hpp
+++ include/maskedcnn/Activation.hpp
@@ -6,7 +6,20 @@ namespace MaskedCNN
 class Activation
 {
 public:
-    Activation();
+    virtual void activate(float *__restrict__ x, float *__restrict__ y, float *__restrict__ delta, int num) = 0;
+};
+
+
+class ReLu : public Activation
+{
+public:
+    virtual void activate(float *__restrict__ x, float *__restrict__ y, float *__restrict__ delta, int num) override;
+};
+
+class Softmax : public Activation
+{
+public:
+    virtual void activate(float *__restrict__ x, float *__restrict__ y, float *__restrict__ delta, int num) override;
 };
 
 }
--- src/Activation.cpp
+++ src/Activation.cpp
@@ -1,1 +1,44 @@
- #include "Activation.hpp"
+#include "Activation.hpp"
+#include <cmath>
+namespace MaskedCNN
+{
+
+void ReLu::activate(float *__restrict__ x, float *__restrict__ y, float *__restrict__ delta, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        y[i] = x[i] > 0.0 ? x[i] : 0.0;
+        delta[i] = x[i] > 0.0 ? 1.0 : 0.0;
+    }
+}
+
+void Softmax::activate(float *__restrict__ x, float *__restrict__ y, float *__restrict__ delta, int num)
+{
+    float maxValue = x[0];
+    for (int i = 1; i < num; i++)
+    {
+        if (x[i] > maxValue)
+        {
+            maxValue = x[i];
+        }
+    }
+
+    float sum = 0;
+    for (int i = 0; i < num; i++)
+    {
+        y[i] = std::exp(x[i] - maxValue);
+        sum += y[i];
+    }
+
+    sum = 1.0 / sum;
+
+    for (int i = 0; i < num; i++)
+    {
+        y[i] *= sum;
+        delta[i] = y[i] * (1 - y[i]);
+    }
+}
+
+}
+
+
