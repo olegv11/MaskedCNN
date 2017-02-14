cb3441ba3aadc58ee778d38ee00482c202aaddc7
Oleg Sikorskiy <olegsik@gmail.com>
2017-02-14 13:30:40 +0000

    Forgot virtual keyword. That's why it wasn't compiling.

--- include/maskedcnn/Layer.hpp
+++ include/maskedcnn/Layer.hpp
@@ -10,7 +10,7 @@ public:
     Layer();
     virtual ~Layer() = default;
     virtual void forwardPropagate(Tensor<float>& input) = 0;
-    //virtual void backwardPropagate(const Tensor<float>& input,  Tensor<float>& prevDelta);
+    virtual void backwardPropagate(const Tensor<float>& input,  Tensor<float>& prevDelta) = 0;
     virtual std::vector<int> getOutputDimensions() = 0;
 
 private:
